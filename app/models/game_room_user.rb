dce4f55b8d799123d4ccddb995964de7a9a15cb0
Keshav Biswa <keshavbiswa21@gmail.com>
2023-02-12 16:40:53 +0530

    Added broadcast to game_room_user

--- app/models/game_room_user.rb
+++ app/models/game_room_user.rb
@@ -3,4 +3,12 @@ class GameRoomUser < ApplicationRecord
   belongs_to :user
 
   validates :user_id, uniqueness: { scope: :game_room_id, message: "should be unique" }
+
+  after_commit :broadcast_create
+
+  private
+
+    def broadcast_create
+      broadcast_replace_to game_room, target: "player_table", partial: 'game_rooms/components/player_table', locals: { game_room:, user:, issue: game_room.current_issue}
+    end
 end
