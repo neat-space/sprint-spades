38d1884d938d3b0cb8dcb265b1d3ca31aa71d8db
Keshav Biswa <keshavbiswa21@gmail.com>
2023-02-19 19:11:07 +0530

    some roles should be left

--- app/models/game_room_user.rb
+++ app/models/game_room_user.rb
@@ -9,6 +9,7 @@ class GameRoomUser < ApplicationRecord
   validates :user_id, uniqueness: { scope: :game_room_id, message: "should be unique" }
 
   after_commit :broadcast_create
+  after_discard :remove_all_roles, :destroy_if_no_game_users_left
 
   private
 
@@ -18,4 +19,18 @@ class GameRoomUser < ApplicationRecord
         broadcast_replace_to game_room, Current.user, target: "player_table", partial: 'game_rooms/components/player_table', locals: { game_room:, user:, issue: game_room.current_issue}
       end
     end
+
+    def remove_all_roles
+      user.roles.where(resource: game_room).destroy_all
+      add_owner_role if game_room.roles.pluck(:name).exclude?("owner")
+    end
+
+    def add_owner_role
+      game_room.game_room_users.kept.first&.user&.add_role(:owner, game_room)
+    end
+    
+    def destroy_if_no_game_users_left
+      game_room.destroy if game_room.game_room_users.kept.empty?
+    end
+
 end
