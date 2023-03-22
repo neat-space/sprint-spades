c43b7ea97bc9ef36b7c704e2b5e17d405e6b67bf
Keshav Biswa <keshavbiswa21@gmail.com>
2023-03-22 20:34:20 +0530

    role_spec

---
+++ spec/factories/roles.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  resource_type :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  resource_id   :bigint
+#
+# Indexes
+#
+#  index_roles_on_name_and_resource_type_and_resource_id  (name,resource_type,resource_id)
+#  index_roles_on_resource                                (resource_type,resource_id)
+#
+FactoryBot.define do
+  factory :role do
+    name { Faker::Lorem.word }
+    resource_type { Faker::Lorem.word }
+    association :resource, factory: :user
+  end
+end
