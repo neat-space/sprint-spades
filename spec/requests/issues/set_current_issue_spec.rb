b260f9c9e4434030af05f570bc1cce9172a10127
Keshav Biswa <keshavbiswa21@gmail.com>
2023-03-23 20:58:46 +0530

    remove comments

--- spec/requests/issues/set_current_issue_spec.rb
+++ spec/requests/issues/set_current_issue_spec.rb
@@ -1,20 +1,5 @@
 require 'rails_helper'
 
-def set_current_issue
-  issue = Issue.find(params[:issue_id])
-  authorize issue
-  @game_room.current_issue = issue
-
-  respond_to do |format|
-    if @game_room.save!
-      format.html { redirect_to game_room_url(@game_room), notice: "Issue was successfully set as current." }
-    else
-      format.html { redirect_to game_room_url(@game_room), notice: "Something went wrong." }
-      format.json { render json: @game_room.errors, status: :unprocessable_entity }
-    end
-  end
-end
-
 RSpec.describe "Issues#set_current_issue", type: :request do
   let(:user) { create(:user) }
   let(:game_room) { create(:game_room) }
